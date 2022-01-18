76eb198fadf5bd2ef20d74677f998d4ab06a8e37
Neeraj Gupta <neeraj76@yahoo.com>
2022-01-18 12:38:39 +0530

    Added wait_for_db command

--- Implementation/backend-microservices/run.cmd
+++ Implementation/backend-microservices/run.cmd
@@ -4,3 +4,11 @@ cd ..
 
 docker-compose build 
 docker-compose run ams sh -c 'django-admin startproject ams .'
+docker-compose run ams sh
+
+
+docker-compose run ams sh -c 'python manage.py test && flake8'
+docker-compose run ams sh -c 'python manage.py flake8'
+
+docker-compose run ams sh -c 'python manage.py startapp core'
+docker-compose run ams sh -c 'python manage.py makemigrations'
